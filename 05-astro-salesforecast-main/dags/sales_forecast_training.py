0d5047a50d4510f81d3c0f879e612595947b1768
Đào Minh Thuấn <53705853+daominhthuan42@users.noreply.github.com>
2026-06-11 01:51:42 +0700

    updated

--- 05-astro-salesforecast-main/dags/sales_forecast_training.py
+++ 05-astro-salesforecast-main/dags/sales_forecast_training.py
@@ -1,6 +1,10 @@
 from datetime import datetime, timedelta
-from airflow import DAG, task
+from airflow.decorators import dag, task
 import sys
 
 # Add include path
 sys.path.append("/usr/local/airflow/include")
+
+default_args = {
+
+}
